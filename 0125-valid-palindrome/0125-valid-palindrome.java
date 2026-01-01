c15b65355f58ea2c0de9dede5e440b7494847298
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-01-01 18:54:12 +0530

    Time: 14 ms (32.46%), Space: 46.6 MB (17.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        s= s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int i=0;
         int j=s.length()-1;
+
         while(i<=j){
             if(s.charAt(i) != s.charAt(j)){
                 return false;
+
             }
             i++;
             j--;
@@ -13,4 +15,3 @@ class Solution {
         return true;
     }
 }
-
