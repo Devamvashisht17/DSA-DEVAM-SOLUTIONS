9bf1e57288d329ceea34d5b5e23112260ecf5374
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-02-18 11:10:07 +0530

    Time: 0 ms (100%), Space: 45.7 MB (46.62%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,29 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> ans= new ArrayList<>();
         backtrack(0, nums, ans);
         return ans;
     }
-    private void backtrack(int ind, int [] nums, List<List<Integer>>ans){
+    private void backtrack(int ind, int [] nums, List<List<Integer>> ans){
         if(ind == nums.length){
-            List<Integer> ds= new ArrayList<>();
+            List<Integer> ds = new ArrayList<>();
             for(int i=0;i<nums.length;i++){
                 ds.add(nums[i]);
-            
-        }
-        ans.add(new ArrayList<>(ds));
+            }
+
+        ans.add(ds);
         return;
     }
     for(int i=ind;i<nums.length;i++){
         swap(i, ind, nums);
         backtrack(ind+1, nums, ans);
-        swap(i,ind, nums);
-
+        swap(i, ind, nums);
     }
+    
     }
-    private void swap(int i, int j, int [] nums){
-        int temp= nums[i];
+    private void swap(int i, int j, int []nums){
+        int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
+
 }
