af21131a6d5f9494e777f49e22bd9c4ff3d0bf56
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-02-24 00:06:58 +0530

    Time: 4 ms (76.71%), Space: 50.2 MB (50.57%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans= new ArrayList<>();
+        for(int i=1;i<=9;i++){
+            search(i, n, ans);
+            
+        }
+        return ans;
+    }
+    private void search(int curr, int n, List<Integer> ans){
+        if(curr>n){
+            return;
+        }
+        ans.add(curr);
+        for(int i=0;i<=9;i++){
+            search(curr *10 +i, n, ans);
+        }
+    }
+}
