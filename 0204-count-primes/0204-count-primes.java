062c59bcde4b2611c41538ae880d5bdfc0010a51
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-03-17 10:31:56 +0530

    Time: 104 ms (49.12%), Space: 51.3 MB (49.73%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,11 +3,10 @@ class Solution {
         if(n<=2){
             return 0;
         }
-        boolean[] isPrime= new boolean[n];
-
-        for(int i=2;i<n;i++){
-            isPrime[i] = true;
-        }
+        boolean [] isPrime = new boolean[n];
+        for(int i=1;i<n;i++){
+            isPrime[i]= true;
+        } 
         for(int i=2;i*i<n;i++){
             if(isPrime[i]){
                 for(int j=i*i;j<n;j=j+i){
@@ -20,9 +19,7 @@ class Solution {
             if(isPrime[i]){
                 count++;
             }
-
         }
         return count;
-
     }
 }
