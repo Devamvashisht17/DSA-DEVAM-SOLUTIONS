57abc5e2f025539e39c2d977791a8f49c8001a51
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-04-16 15:13:03 +0530

    Time: 0 ms (100%), Space: 45 MB (98.84%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans= new ArrayList<>();
-        backtrack(0, nums, ans);
+        recurpermute(0, nums, ans);
         return ans;
     }
-    private void backtrack(int ind, int [] nums, List<List<Integer>> ans){
-        if(ind == nums.length){
-            List<Integer> ds = new ArrayList<>();
+    public void recurpermute(int ind, int [] nums, List<List<Integer>> ans){
+        if(ind== nums.length){
+            List<Integer> ds= new ArrayList<>();
             for(int i=0;i<nums.length;i++){
                 ds.add(nums[i]);
             }
-
-        ans.add(ds);
-        return;
+            ans.add(ds);
+            return;
+        }
+        for(int i =ind;i<nums.length;i++){
+            swap(i,ind, nums);
+            recurpermute(ind+1, nums, ans);
+            swap(i, ind, nums);
+        }
     }
-    for(int i=ind;i<nums.length;i++){
-        swap(i, ind, nums);
-        backtrack(ind+1, nums, ans);
-        swap(i, ind, nums);
-    }
-    
-    }
-    private void swap(int i, int j, int []nums){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int i, int j, int [] nums){
+        int temp=nums[i];
+        nums[i]= nums[j];
+        nums[j]= temp;
     }
 
 }
