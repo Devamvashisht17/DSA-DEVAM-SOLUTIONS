4504818ac9f46aafb6f091586cdc4bed898f0163
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-04-22 12:00:19 +0530

    Time: 1 ms (6.8%), Space: 43 MB (71.98%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,18 +15,22 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list= new ArrayList<>();
-        preorder(root, list);
-        return list;
-    }
-    public void preorder(TreeNode root, List<Integer> list){
-        if(root== null){
-            return;
+        List<Integer> preorder= new ArrayList<>();
+        if(root == null){
+            return preorder;
         }
-        list.add(root.val);
-        preorder(root.left, list);
-        preorder(root.right, list);
-
-
-    } 
+        Stack<TreeNode> st= new Stack <TreeNode>();
+        st.push(root);
+        while(!st.isEmpty()){
+            root=st.pop();
+            preorder.add(root.val);
+            if(root.right != null){
+                st.push(root.right);
+            }
+            if(root.left != null){
+                st.push(root.left);
+            }
+        }
+        return preorder;
+    }
 }
