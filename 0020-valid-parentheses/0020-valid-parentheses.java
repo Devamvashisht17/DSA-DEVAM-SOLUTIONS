617cf6aa466a27489b1a6fbcd74aa5a8318465f4
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-05-10 00:25:10 +0530

    Time: 3 ms (87.66%), Space: 43.5 MB (15.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,21 +3,22 @@ class Solution {
         Stack<Character> st= new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch= s.charAt(i);
-
             if(ch == '[' || ch == '(' || ch == '{'){
                 st.push(ch);
             }
             else{
-                if(st.empty()){
+                if(st.isEmpty()){
                     return false;
                 }
-                char c = st.pop();
-                if(c == '[' && ch == ']') continue;
-                if(c == '(' && ch == ')')continue;
-                if(c == '{' && ch == '}') continue;
+                char c= st.pop();
+                if (c== '[' && ch == ']') continue;
+                if (c =='(' && ch == ')') continue;
+                if(c=='{' && ch== '}') continue;
+
                 return false;
             }
         }
-        return st.empty();
-    }
+            return st.empty();
+
+        }
 }
