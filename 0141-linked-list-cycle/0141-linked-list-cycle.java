ba902c265524007f5231a2605730478bbac661dc
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-06-11 09:04:57 +0530

    Time: 0 ms (100%), Space: 46.6 MB (71.15%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,12 +15,14 @@ public class Solution {
         ListNode slow= head;
 
         while(fast !=null && fast.next !=null){
-            fast= fast.next.next;
-            slow= slow.next;
-            if(slow==fast){
-                return true;
-            }
+            fast=fast.next.next;
+            slow=slow.next;
+
+                if(slow==fast){
+                    return true;
+                }
         }
         return false;
+        
     }
 }
