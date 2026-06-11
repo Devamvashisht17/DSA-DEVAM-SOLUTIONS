8928e83ab21a2f1831d21f34f672509f02c6f42d
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-06-11 09:41:25 +0530

    Time: 52 ms (66.36%), Space: 143.2 MB (55.61%) - LeetHub

---
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        Queue<TreeNode> q= new LinkedList<>();
+        ArrayList<Long> arr= new ArrayList<>();
+
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size= q.size();
+            long sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                sum=sum+node.val;
+                
+                if(node.left !=null){
+                    q.offer(node.left);
+                }
+                if(node.right !=null){
+                    q.offer(node.right);
+                }
+            }
+            arr.add(sum);
+        }
+        Collections.sort(arr);
+        if(arr.size()<k)
+        return -1;
+        return arr.get(arr.size()-k);
+    }
+}
