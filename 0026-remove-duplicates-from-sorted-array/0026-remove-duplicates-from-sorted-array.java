79d5997317fae78e6e5f4bfb7ea3d255c8d74b59
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-06-14 14:27:23 +0530

    Time: 1 ms (77.21%), Space: 46.9 MB (23.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -5,7 +5,8 @@ class Solution {
 
         for(int i=0;i<=n;i++){
             if(nums[i] != nums[j-1]){
-                nums[j++]= nums[i];
+                nums[j]= nums[i];
+                j++;
             }
         }
         return j;
