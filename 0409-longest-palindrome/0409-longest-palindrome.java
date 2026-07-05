b8d92e960bbc0c484f783afd23dfc7d57e16d6d3
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-07-05 18:15:25 +0530

    Time: 7 ms (49.08%), Space: 43.5 MB (12.13%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character>set = new HashSet<>();
+        int length=0;
+        for(char ch: s.toCharArray()){
+            if(set.contains(ch)){
+                set.remove(ch);
+                length=length+2;
+            }
+            else{
+                set.add(ch);
+            }
+        }
+        if(!set.isEmpty()){
+            length=length+1;
+        }
+        return length;
+
+
+    }
+}
