13e6e923486c0cf9df36c3e89453a89e020879f7
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-08-02 12:50:43 +0530

    Time: 0 ms (100%), Space: 43.3 MB (9.58%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> list= new ArrayList<>();
 
         int top=0;
         int bottom= matrix.length-1;
         int left=0;
-        int right= matrix[0].length-1;
+        int right=matrix[0].length-1;
         while(top<=bottom && left<=right){
-
-        for(int i=left;i<=right;i++){
-            ans.add(matrix[top][i]);
-        }
-        top++;
-
-        for(int i=top;i<=bottom;i++){
-            ans.add(matrix[i][right]);
-        }
-        right--;
-
-        if(top<=bottom){
-            for(int i=right;i>=left;i--){
-                ans.add(matrix[bottom][i]);
+            for(int i=left;i<=right;i++){
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                list.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    list.add(matrix[bottom][i]);
+                }
             }
             bottom--;
-        }
-        if(left<=right){
-            for(int i=bottom;i>=top;i--){
-                ans.add(matrix[i][left]);
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    list.add(matrix[i][left]);
+                }
             }
             left++;
         }
-        }
-        return ans;
+        return list;
     }
 }
