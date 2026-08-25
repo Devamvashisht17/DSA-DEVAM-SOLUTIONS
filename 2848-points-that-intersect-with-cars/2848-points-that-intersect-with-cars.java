cfccaddd0aab2e5586053086e42177c66bd46fe2
Devamvashisht17 <devamvashisht1704@gmail.com>
2026-08-25 20:48:18 +0530

    Time: 5 ms (29.7%), Space: 46.5 MB (33.03%) - LeetHub

---
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        HashSet<Integer> set= new HashSet<>();
+
+        for(List<Integer> car : nums){
+            int start= car.get(0);
+            int end= car.get(1);
+
+            for(int i=start;i<=end;i++){
+                set.add(i);
+            }
+
+        }
+        return set.size();
+    }
+}
